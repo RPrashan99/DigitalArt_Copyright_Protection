1a5eeb975994585dc475ca1054b234f3ca233128
RPrashan99 <RashmithaMK@gmail.com>
2026-02-10 21:54:57 +0530

    add transfer ownership function and tests

--- blockchain/contracts/Copyright.sol
+++ blockchain/contracts/Copyright.sol
@@ -10,6 +10,14 @@ contract Copyright {
 
     mapping(bytes32 => Art) private artRegistry;
 
+    // Track transfers
+    event OwnershipTransferred(
+        bytes32 indexed artHash,
+        address indexed previousOwner,
+        address indexed newOwner,
+        uint256 timestamp
+    );
+
     function registerArt(bytes32 _artHash, string memory _ipfsHash) public {
         require(artRegistry[_artHash].owner == address(0), "Art already registered");
         artRegistry[_artHash] = Art(msg.sender, _ipfsHash);
@@ -19,5 +27,17 @@ contract Copyright {
         Art memory art = artRegistry[_artHash];
         require(art.owner != address(0), "Art not registered");
         return art.owner;
+    }
+
+    function transferOwner(bytes32 _artHash, address _newOwner) public {
+        Art storage art = artRegistry[_artHash];
+        require(art.owner != address(0), "Art not registered");
+        require(art.owner == msg.sender, "Only owner can transfer");
+        require(_newOwner != address(0), "New owner cannot be zero address");
+
+        address previousOwner = art.owner;
+        art.owner = _newOwner;
+        
+        emit OwnershipTransferred(_artHash, previousOwner, _newOwner, block.timestamp);
     }
 }
--- blockchain/test/Copyright.js
+++ blockchain/test/Copyright.js
@@ -52,4 +52,34 @@ describe("Token contract", function () {
         ).to.be.revertedWith("Art already registered");
     });
 
+    it("Should transfer ownership of art", async function () {
+        const artHash = ethers.keccak256(ethers.toUtf8Bytes("ArtPiece1"));
+        const ipfsHash = "QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco";
+
+        await copyright.registerArt(artHash, ipfsHash);
+        await copyright.transferOwner(artHash, addr1.address);
+
+        const newOwnerAddress = await copyright.verifyArt(artHash);
+        expect(newOwnerAddress).to.equal(addr1.address);
+    })
+
+    it("Should fail to transfer ownership of art by non-owner", async function () {
+        const artHash = ethers.keccak256(ethers.toUtf8Bytes("ArtPiece1"));
+        const ipfsHash = "QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco";
+
+        await copyright.registerArt(artHash, ipfsHash);
+        await expect(
+            copyright.connect(addr1).transferOwner(artHash, addr1.address)
+        ).to.be.revertedWith("Only owner can transfer");
+    });
+
+    it("Should fail to transfer ownership of art that no registered", async function () {
+        const artHash = ethers.keccak256(ethers.toUtf8Bytes("ArtPiece1"));
+        const ipfsHash = "QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco";
+
+        await expect(
+            copyright.transferOwner(artHash, addr1.address)
+        ).to.be.revertedWith("Art not registered");
+    })
+
 });
